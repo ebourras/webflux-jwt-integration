2f2ce09f4b12f5ff1bee50c613cec5e8530ee753
Bourass El-Mehdi <el-mehdi.bourass@inetum.com>
2024-07-26 13:24:19 +0200

    Update Customer Delivery Feature

--- src/main/java/com/deliveryapp/delivery/repository/CustomerDeliveryRepository.java
+++ src/main/java/com/deliveryapp/delivery/repository/CustomerDeliveryRepository.java
@@ -1,10 +1,13 @@
 package com.deliveryapp.delivery.repository;
 
 import com.deliveryapp.delivery.model.CustomerDelivery;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 public interface CustomerDeliveryRepository extends ReactiveCrudRepository<CustomerDelivery, String> {
 
-    Flux<CustomerDelivery> findByCustomerId(String customerId);
+    @Query("{ 'customerId' : ?0 }")
+    Mono<CustomerDelivery> findCustomerDeliveryByCustomerId(@Param("customerId") String customerId);
 }
